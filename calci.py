627a09fa26b2426d141603968451e40427b95e6d
Anne George <itsannegeorge@gmail.com>
2025-12-18 20:32:28 +0530

    made basic calci with addition

---
+++ calci.py
@@ -0,0 +1,8 @@
+a= int (input("Enter first number"))
+b= int (input("Enter second number"))   
+print("1. Addition 2Subtraction")
+choice = int (input("Enter your choice"))
+if choice == 1:
+    print("Addition is", a+b)
+elif choice == 2:
+    print("Subtraction not done yet")
