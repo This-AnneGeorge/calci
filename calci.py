6f85596eb665bad52c5e449ef7e51b13de7aadb0
Anne George <itsannegeorge@gmail.com>
2025-12-18 20:42:29 +0530

    broken

--- calci.py
+++ calci.py
@@ -3,6 +3,6 @@ b= int (input("Enter second number"))
 print("1. Addition 2Subtraction")
 choice = int (input("Enter your choice"))
 if choice == 1:
-    print("Addition is", a+b)
+    print("Addition is", a*b)
 elif choice == 2:
     print("Subtraction is", a-b)
